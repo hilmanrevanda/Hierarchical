f96927a6d3ec7255fcca591c5edf7a17ae19d8d7
Predrika Br Ginting <ichapredrikagm@gmail.com>
2018-03-03 12:23:11 +0700

    - Storing point, line, object,list of object as class

--- Hierarchical/Form1.Designer.vb
+++ Hierarchical/Form1.Designer.vb
@@ -22,17 +22,45 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pbCanvas = New System.Windows.Forms.PictureBox()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pbCanvas, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'pbCanvas
+        '
+        Me.pbCanvas.Location = New System.Drawing.Point(13, 13)
+        Me.pbCanvas.Name = "pbCanvas"
+        Me.pbCanvas.Size = New System.Drawing.Size(580, 425)
+        Me.pbCanvas.TabIndex = 0
+        Me.pbCanvas.TabStop = False
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(627, 13)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 1
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(968, 450)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.pbCanvas)
         Me.Name = "Form1"
         Me.Text = "Hierarchical Model"
+        CType(Me.pbCanvas, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents pbCanvas As PictureBox
+    Friend WithEvents btnReset As Button
+    Friend WithEvents Timer1 As Timer
 End Class
--- Hierarchical/Form1.vb
+++ Hierarchical/Form1.vb
@@ -1,5 +1,48 @@
 ﻿Public Class Form1
+    Dim BMP As New Drawing.Bitmap(650, 400)
+    Dim GFX As Graphics = Graphics.FromImage(BMP)
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+End Class
+
+Public Class Point 'coordinates of object in OCS
+    Public x, y, z, w As Double
+    Sub New(a As Double, b As Double, c As Double, d As Double)
+        x = a
+        y = b
+        z = c
+        w = d 'transformation
+    End Sub
+End Class
+
+Public Class Line 'indexes of start point and end point of the line
+    Public P1, P2 As Integer
+    Sub New(d1 As Integer, d2 As Integer)
+        P1 = d1
+        P2 = d2
+    End Sub
+End Class
+
+Public Class Object3D 'Cuboid contains array of vertices and array of edges
+    Public vertices(7) As Point
+    Public edges(11) As Line
+    Sub SetPoint(index As Integer, x As Double, y As Double, z As Double)
+        vertices(index) = New Point(x, y, z, 1)
+    End Sub
+
+End Class
+
+Public Class List3DObject
+    Public First As ElmtList3DObject
+End Class
+
+Public Class ElmtList3DObject
+    Public Child As List3DObject
+    Public Nxt As ElmtList3DObject
+    Public Axisrot As Char
+    Public alpha As Double
+    Public Obj As Object3D
+    Public Transform(3, 3) As Double
+
 End Class
